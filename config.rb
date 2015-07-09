2be28d24ec367bdd8de4c530a9817e3725926b52
David Myers <davidmyers9000@gmail.com>
2015-07-09 13:12:10 -0400

    configure deploy

--- config.rb
+++ config.rb
@@ -85,6 +85,15 @@ set :js_dir, 'javascripts'
 set :images_dir, 'images'
 
 # Build-specific configuration
+activate :deploy do |deploy|
+  deploy.method = :git
+  # Optional Settings
+  # deploy.remote   = 'blog' # remote name or git url, default: origin
+  # deploy.branch   = 'custom-branch' # default: gh-pages
+  # deploy.strategy = :submodule      # commit strategy: can be :force_push or :submodule, default: :force_push
+  # deploy.commit_message = 'custom-message'      # commit message (can be empty), default: Automated commit at `timestamp` by middleman-deploy `version`
+end
+
 configure :build do
   # For example, change the Compass output style for deployment
   # activate :minify_css
